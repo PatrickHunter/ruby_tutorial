b1f905afce5d3c09d74db9d2142db5140ff1b0fc
Patrick <pk_hunter@sbcglobal.net>
2013-11-12 17:20:12 -0600

    Add sendgrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ SampleApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => "arcane-springs-5301.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "smtp.sendgrid.net" }
 end
