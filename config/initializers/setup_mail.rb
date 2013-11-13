42d28adc1980705f8d579b69ba23e4c8959c569c
Patrick <pk_hunter@sbcglobal.net>
2013-11-12 21:46:30 -0600

    update settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,6 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "arcane-springs-5301.herokuapp.com/",
   :user_name            => "no-reply@arcane-springs-5301.herokuapp.com/",
-  :password             => "kdsasdfwefsdd",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
