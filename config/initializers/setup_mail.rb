3c561795ab50936b4ac3aed9428b43191ba60808
Patrick <pk_hunter@sbcglobal.net>
2013-11-12 22:01:07 -0600

    More email changes

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,7 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.sendgrid.net",
-  :port                 => 587,
-  :domain               => "arcane-springs-5301.herokuapp.com/",
-  :user_name            => "no-reply@arcane-springs-5301.herokuapp.com/",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+ActionMailer::Base.smtp_settings = { :address   => "smtp.sendgrid.net",
+                           :port      => 587,
+                           :domain    => "arcane-springs-5301.herokuapp.com",
+                           :user_name => "no-reply@arcane-springs-5301.herokuapp.com",
+                           :authentication => 'plain',
+                           :enable_starttls_auto => true 
+                           }
