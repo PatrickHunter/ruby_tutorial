d47b2c51201cbb19183eccdf3061be71f1970e9b
Patrick <pk_hunter@sbcglobal.net>
2013-11-12 22:32:24 -0600

    Changed password experation time

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,7 +14,7 @@ class PasswordResetsController < ApplicationController
   
   def update
     @user = User.find_by_password_reset_token!(params[:id])
-    if @user.password_reset_sent_at > 2.hours.ago
+    if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Sorry thats gone stale."
     elsif @user.update_attributes(params.require(:user).permit(:password, :password_confirmation))
       redirect_to root_url, :notice => "Reset complete!"
