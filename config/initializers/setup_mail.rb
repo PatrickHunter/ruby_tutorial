a7c1b748fd670017960d373565b8c652a9084ac4
Patrick <pk_hunter@sbcglobal.net>
2013-11-12 19:39:44 -0600

    Mail setup

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.sendgrid.net",
+  :port                 => 587,
+  :domain               => "arcane-springs-5301.herokuapp.com/",
+  :user_name            => "no-reply@arcane-springs-5301.herokuapp.com/",
+  :password             => "kdsasdfwefsdd",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
